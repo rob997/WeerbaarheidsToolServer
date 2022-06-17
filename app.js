06a25ebb54ff3d84b1df4b622372a3a15a4c232d
Ubuntu <rob@Express.5wjlnyyaexxeddnkltxb0bhqyd.cwx.internal.cloudapp.net>
2022-06-17 18:18:17 +0000

    Added email variable

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.get('/getAPIResponse', (req, res) => {
 })
 
 app.get('/getEmailBreaches', (req, res) => {
-	api_helper.make_API_call_key('https://haveibeenpwned.com/api/v3/breachedaccount/','robperdok@hotmail.com', '18b7abde56c449f4aabbdfae21285417')
+	let email = req.query.email;
+
+	api_helper.make_API_call_key('https://haveibeenpwned.com/api/v3/breachedaccount/', email, '18b7abde56c449f4aabbdfae21285417')
 	.then(response => {
 		res.json(response)
 	})
