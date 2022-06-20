add231c0105d533320db7596f8ee79cde5f5e3cd
Rob <robperdok@hotmail.com>
2022-06-20 22:20:36 +0200

    addUser test

--- app.js
+++ app.js
@@ -1,33 +1,81 @@
-const express = require('express')
-const api_helper = require('./API_helper')
-const app = express()
-const port = 3000
+const express = require("express");
+const api_helper = require("./API_helper");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Welcome to Make REST API Calls In Express!'))
+// Database connection
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "react",
+  password: "Weer-Baar-Heid33",
+  database: "mydb",
+});
+connection.connect();
 
-app.get('/getAPIResponse', (req, res) => {
-	api_helper.make_API_call('https://jsonplaceholder.typicode.com/todos/1')
-	.then(response => {
-		res.json(response)
-	})
-	.catch(error => {
-		res.send(error)
-	})
-})
+// Database queries
+connection.query(
+  "INSERT INTO User (ID, SharesInfo) VALUES 1, 'Ja'",
+  function (err, fields) {
+    if (err) throw err;
+    console.log("Succes!");
+  }
+);
 
-app.get('/getEmailBreaches', (req, res) => {
-	let email = req.query.email;
+app.post("/addUser", function(req, res) {
+    // get data from forms and add to the table called user..
 
-	api_helper.make_API_call_key('https://haveibeenpwned.com/api/v3/breachedaccount/', email, '18b7abde56c449f4aabbdfae21285417')
-	.then(response => {
-		res.json(response)
-	})
-	.catch(error => {
-		res.send(error)
-	})
-})
+    let reqBody = req.body;
+
+    const name = reqBody.id;
+    const email = reqBody.sharesinfo;
+
+    const queryString = `INSERT INTO User (ID, SharesInfo) VALUES (1, 'Ja')`;
+
+    connection.query(queryString, function (err, result) {
+    if (err) {
+        // Throw your error output here.
+        console.log("An error occurred.");
+    } else {
+        // Throw a success message here.
+        console.log("1 record successfully inserted into db");
+    }
+  });
 
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get("/", (req, res) =>
+  res.send("Welcome to Make REST API Calls In Express!")
+);
 
-module.exports = app
+app.get("/getAPIResponse", (req, res) => {
+  api_helper
+    .make_API_call("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+// Request email information from API
+app.get("/getEmailBreaches", (req, res) => {
+  let email = req.query.email;
+
+  api_helper
+    .make_API_call_key(
+      "https://haveibeenpwned.com/api/v3/breachedaccount/",
+      email,
+      "18b7abde56c449f4aabbdfae21285417"
+    )
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+module.exports = app;
