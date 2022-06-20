4aea22d8a99ce21108bc698f08a7b5951a981d16
Rob <robperdok@hotmail.com>
2022-06-20 22:27:28 +0200

    add user test

--- app.js
+++ app.js
@@ -14,35 +14,49 @@ const connection = mysql.createConnection({
 connection.connect();
 
 // Database queries
-connection.query(
-  "INSERT INTO User (ID, SharesInfo) VALUES 1, 'Ja'",
-  function (err, fields) {
-    if (err) throw err;
-    console.log("Succes!");
-  }
-);
+//connection.query(
+//  "INSERT INTO User (ID, SharesInfo) VALUES 1, 'Ja'",
+//  function (err, fields) {
+//    if (err) throw err;
+//    console.log("Succes!");
+//  }
+//);
 
-app.post("/addUser", function(req, res) {
-    // get data from forms and add to the table called user..
+// app.post("/addUser", function(req, res) {
+//     // get data from forms and add to the table called user..
 
-    let reqBody = req.body;
+//     let reqBody = req.body;
 
-    const name = reqBody.id;
-    const email = reqBody.sharesinfo;
+//     const name = reqBody.id;
+//     const email = reqBody.sharesinfo;
 
-    const queryString = `INSERT INTO User (ID, SharesInfo) VALUES (1, 'Ja')`;
+//     const queryString = `INSERT INTO User (ID, SharesInfo) VALUES (1, 'Ja')`;
 
-    connection.query(queryString, function (err, result) {
+//     connection.query(queryString, function (err, result) {
+//     if (err) {
+//         // Throw your error output here.
+//         console.log("An error occurred.");
+//     } else {
+//         // Throw a success message here.
+//         console.log("1 record successfully inserted into db");
+//     }
+//   });
+//}
+
+app.post("/addUser", (req, res) => {
+  // get data from forms and add to the table called user..
+  const query = `INSERT INTO User (ID, SharesInfo) VALUES (1, 'Ja')`;
+
+  connection.query(query, function (err, result) {
     if (err) {
-        // Throw your error output here.
-        console.log("An error occurred.");
+      // Throw your error output here.
+      console.log("An error occurred.");
     } else {
-        // Throw a success message here.
-        console.log("1 record successfully inserted into db");
+      // Throw a success message here.
+      console.log("1 record successfully inserted into db");
     }
   });
-}
-
+});
 
 app.get("/", (req, res) =>
   res.send("Welcome to Make REST API Calls In Express!")
